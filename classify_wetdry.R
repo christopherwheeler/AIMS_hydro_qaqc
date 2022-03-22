904d4be10509e8b84992de7ca957e66ba7c63bfe
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-03-22 11:10:50 -0500

    first draft working function (just absolute threshold for now)

--- classify_wetdry.R
+++ classify_wetdry.R
@@ -3,25 +3,10 @@
 # inputs: data frame with columns "datetime", "temperature", "conductivity", and "specific_conductivity"
 # output: same data frame as input, but with a new column called wetdry
 
-stic_data <- stic_data %>% 
-  mutate(specific_conductivity = conductivity/80)
-
 classify_wetdry <- function(input, threshold = 1000) {
   
 input <- input %>% 
-  dplyr::mutate(wetdry = if_else(measured_cond >= threshold, "wet", "dry" ))
+  dplyr::mutate(wetdry = if_else(conductivity_uncal >= threshold, "wet", "dry" ))
 
 }
-
-
-# create dummy data to test this since I don't have apply_calibration working 
-stic_data <- stic_data %>% 
-  mutate(specific_cond = measured_cond/80)
-
-classified <- classify_wetdry(stic_data, 500)
-
-
-
-
-
 
