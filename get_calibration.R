87d6d7a90e00e6914f1e71bdfc0abbfa531c767e
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-03-21 20:04:03 -0500

    fixed error message

--- get_calibration.R
+++ get_calibration.R
@@ -7,15 +7,18 @@
 
 get_calibration <- function(calibration_data, method = "linear") {
   
-  calibration <-  lm(standard ~ conductivity_uncal, data = calibration_data)
+  if (method == "exponential") {
   
-  if(method == "exponential") {
-    
     calibration <- lm(log(standard) ~ conductivity_uncal, data = calibration_data)
-  } else{
-    stop("This method is unknown. Please use the linear or exponential method")
+    
+  } else if (method == "linear") {
+    
+    calibration <-  lm(standard ~ conductivity_uncal, data = calibration_data)
+    
+  } else {
+    
+    calibration <- print("Unknown method. Please use linear of exponential.")
   }
   
   return(calibration)  
 }
-
