6d7527f95c25cc0763c752ee59841e25f6ddcbe0
Christopher Wheeler <christopher.wheeler@ku.edu>
2022-09-24 13:02:51 -0500

    testing

--- stic_threshold_exp.R
+++ stic_threshold_exp.R
@@ -1,4 +1,4 @@
-#### CTW - Feb 2022
+#### CTW - Feb 2022 - testing git
 #### Importing multiple "Clean" STIC files from the same folder and binding
 ### Creating binary wet/dry dataset from relative conductivity
 ### Determining basic flow metrics
@@ -28,7 +28,7 @@ stic_files <- stic_files %>%
   map_dfr(read_csv)
 
 ### Creating binary wet/dry column based of relative cond value of 1000
-stic_files <- stic_files %>% 
+stic_files_b <- stic_files %>% 
   dplyr::mutate(wetdry1000 = if_else(conductivity >= 1000, "wet", "dry" )) %>% 
   dplyr::mutate(wetdry200 = if_else(conductivity >= 200, "wet", "dry" )) %>% 
   dplyr::mutate(wetdry400 = if_else(conductivity >= 400, "wet", "dry" )) %>% 
@@ -38,21 +38,21 @@ stic_files <- stic_files %>%
 
 
 ### Making another datetime column in tidy date format 
-stic_files <- stic_files %>% 
+stic_files_b <- stic_files_b %>% 
   mutate(date = lubridate::date(datetime))
 
 ### NOTE: this command is just trimming the date range to the time when all the loggers were active
 ### It is not something that is always neccessary
-stic_files <- stic_files%>% 
+stic_files_b <- stic_files_b %>% 
   dplyr::filter(datetime >= "2021-05-23")
 
 ### using lubridate to separate out the individual days 
-stic_files <- stic_files %>% 
+stic_files_b <- stic_files_b %>% 
   mutate(day = lubridate::yday(date))
 
 
 ### making a new dataframe with date of first drying for all individual logger
-drying <- stic_files %>% 
+drying <- stic_files_b %>% 
   subset(wetdry == "dry") %>%
   group_by(logger) %>% 
   summarise(first_dry = min(date)) 
@@ -63,7 +63,7 @@ drying <- stic_files %>%
 
 ### Calculate wet network proportion
 
-stic_wet_prop <- stic_files %>% 
+stic_wet_prop <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry == "wet"), n_sensors = n() ) %>% 
   mutate(percent = n_wet/n_sensors)
@@ -72,7 +72,7 @@ stic_wet_prop <- stic_files %>%
 ### Calculate flow duration for each sensor
 ### i,e, percentage of wet observations out of total obs for each logger
 
-high_stic_duration <- stic_files %>% 
+high_stic_duration <- stic_files_b %>% 
   group_by(logger) %>% 
   summarise(n_wet = sum(wetdry == "wet", na.rm = TRUE), count = n()) %>% 
   mutate(duration = n_wet/count)
@@ -80,28 +80,28 @@ high_stic_duration <- stic_files %>%
 ############################################################################################################################
 rm(stic_wet_prop)
 
-stic_wet_prop1000 <- stic_files %>% 
+stic_wet_prop1000 <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry1000 == "wet"), n_sensors = n() ) %>% 
   mutate(percent1000 = n_wet/n_sensors)
 
-stic_wet_prop200 <- stic_files %>% 
+stic_wet_prop200 <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry200 == "wet"), n_sensors = n() ) %>% 
   mutate(percent200 = n_wet/n_sensors)
 
-stic_wet_prop400 <- stic_files %>% 
+stic_wet_prop400 <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry400 == "wet"), n_sensors = n() ) %>% 
   mutate(percent400 = n_wet/n_sensors)
 
-stic_wet_prop600 <- stic_files %>% 
+stic_wet_prop600 <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry600 == "wet"), n_sensors = n() ) %>% 
   mutate(percent600 = n_wet/n_sensors)
 
 
-stic_wet_prop800 <- stic_files %>% 
+stic_wet_prop800 <- stic_files_b %>% 
   group_by(datetime) %>% 
   summarise(n_wet = sum(wetdry800 == "wet"), n_sensors = n() ) %>% 
   mutate(percent800 = n_wet/n_sensors)
@@ -142,4 +142,175 @@ ggplot(threshold) +
   geom_path(aes(x = datetime, y = percent1000), color = "red") +
   geom_path(aes(x = datetime, y = percent200), color = "blue") + 
   theme_cowplot()
+
+
+#==============================================================================================================================
+# Now try thresholds with actual SPC
+
+
+source("get_calibration.R")
+
+source("apply_calibration.R")
   
+## Step 2: Get calibration
+
+cal_points <- read_csv("cal_points.csv")
+
+cal_points <- cal_points %>% 
+  rename(standard = std_val) %>% 
+  rename(conductivity_uncal = measured_val)
+
+calibration_data <- cal_points
+
+calibration <- get_calibration(calibration_data, method = "linear")
+
+
+## Step 3: Apply calibration
+
+stic_files <- stic_files %>% 
+  rename(conductivity_uncal = conductivity)
+
+stic_data_with_spc <- apply_calibration(input = stic_files, calibration = calibration)
+
+
+
+### Creating binary wet/dry column based of relative cond value of 1000
+stic_data_with_spc <- stic_data_with_spc %>% 
+  dplyr::mutate(wetdry50 = if_else(spc >= 50, "wet", "dry" )) %>% 
+  dplyr::mutate(wetdry100 = if_else(spc >= 100, "wet", "dry" )) %>% 
+  dplyr::mutate(wetdry200 = if_else(spc >= 200, "wet", "dry" )) %>% 
+  dplyr::mutate(wetdry300 = if_else(spc >= 300, "wet", "dry" )) %>% 
+  dplyr::mutate(wetdry400 = if_else(spc >= 400, "wet", "dry" )) %>% 
+  dplyr::mutate(wetdry500 = if_else(spc >= 500, "wet", "dry" )) 
+
+
+
+### Making another datetime column in tidy date format 
+stic_data_with_spc <- stic_data_with_spc %>% 
+  mutate(date = lubridate::date(datetime))
+
+### NOTE: this command is just trimming the date range to the time when all the loggers were active
+### It is not something that is always neccessary
+stic_data_with_spc <- stic_data_with_spc %>% 
+  dplyr::filter(datetime >= "2021-05-23")
+
+### using lubridate to separate out the individual days 
+stic_data_with_spc <- stic_data_with_spc %>% 
+  mutate(day = lubridate::yday(date))
+
+
+### making a new dataframe with date of first drying for all individual logger
+drying <- stic_data_with_spc %>% 
+  subset(wetdry == "dry") %>%
+  group_by(logger) %>% 
+  summarise(first_dry = min(date)) 
+
+
+
+### Now calculating the percentage of wet STICs at each time step
+
+
+
+stic_wet_prop50 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry50 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent50 = n_wet/n_sensors)
+
+stic_wet_prop100 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry100 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent100 = n_wet/n_sensors)
+
+stic_wet_prop200 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry200 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent200 = n_wet/n_sensors)
+
+stic_wet_prop300 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry300 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent300 = n_wet/n_sensors)
+
+
+stic_wet_prop400 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry400 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent400 = n_wet/n_sensors)
+
+stic_wet_prop500 <- stic_data_with_spc %>% 
+  group_by(datetime) %>% 
+  summarise(n_wet = sum(wetdry500 == "wet"), n_sensors = n() ) %>% 
+  mutate(percent500 = n_wet/n_sensors)
+
+
+threshold_spc <- left_join(stic_wet_prop50, stic_wet_prop100, by = "datetime")
+
+thresh2_spc <- left_join(threshold_spc, stic_wet_prop200, by = "datetime")
+
+thresh3_spc <- left_join(thresh2_spc, stic_wet_prop300, by = "datetime")
+
+thresh4_spc <- left_join(thresh3_spc, stic_wet_prop400, by = "datetime")
+
+thresh5_spc <- left_join(thresh4_spc, stic_wet_prop500, by = "datetime")
+
+write_csv(thresh5_spc, "threshold_spc.csv")
+
+#############################################################################################################################
+#############################################################################################################################
+
+rm(threshold_stic)
+
+threshold_stic <- read_csv("threshold_stic.csv")
+
+threshold_stic <- threshold_stic %>% 
+  rename(percent_wet = percent1000)
+
+
+ggplot(threshold_stic, aes(x = datetime, y = percent_wet, color = Category)) + 
+  geom_path() + 
+  theme_cowplot()
+
+
+threshold <- read_csv("threshold.csv")
+
+
+ggplot(threshold) + 
+  geom_path(aes(x = datetime, y = percent1000), color = "red") +
+  geom_path(aes(x = datetime, y = percent200), color = "blue") + 
+  theme_cowplot()
+
+#=============================================================================================================================
+
+
+threshold_spc <- read_csv("threshold_spc.csv")
+
+
+threshold_spc_s <- threshold_spc %>% 
+  select(datetime, percent50, percent100, percent200, percent300, percent400, percent500)
+
+
+
+ggplot(threshold_spc_s) + 
+  geom_line(aes(x = datetime, y = percent50), color = "red") + 
+  geom_line(aes(x = datetime, y = percent100), color = "blue") + 
+  geom_line(aes(x = datetime, y = percent200), color = "green") + 
+  geom_line(aes(x = datetime, y = percent300), color = "yellow") + 
+  geom_line(aes(x = datetime, y = percent400), color = "steelblue") + 
+  geom_line(aes(x = datetime, y = percent500), color = "magenta") + 
+  theme_cowplot() + 
+  ylab("wet network proportion based on spc")
+
+
+# USE 50 SPC
+ 
+daily_stic <- stic_data_with_spc %>% 
+  select(logger, datetime, date, temperature, conductivity_uncal, spc, wetdry50)
+
+stic_daily <- daily_stic %>% 
+  group_by(logger, date) %>% 
+  summarize(daily_temp = mean(temperature),
+            daily_spc = mean(spc))
+
+write_csv(stic_daily, "daily_stic.csv")
+
+
